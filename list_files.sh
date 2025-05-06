e5d07f6a788f963078e1c7854a42cf5f93130cc9
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-06 14:07:58 +0100

    ajout du script list_files.sh

---
+++ list_files.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Ce script affiche la liste des fichiers dans le répertoire courant
+
+# Utilisation de la commande `ls` pour lister les fichiers
+echo "Liste des fichiers dans le répertoire courant :"
+ls -l
