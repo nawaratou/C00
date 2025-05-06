89e4280c41ac75ac22bef9e13139887d820b2771
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-07 00:02:48 +0100

    afficher le script

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Ce script vérifie si un fichier passé en argument existe ou non
 
-if [ -f "$1" ]; then
+[ -f "$1" ]; then
     echo "Le fichier '$1' existe."
-else
+
     echo "Le fichier '$1' n'existe pas."
-fi
+
