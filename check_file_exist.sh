043f387010bc6b4055dd5e0c0ebfae5aa460d9ad
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-06 14:21:58 +0100

    ajout du script check_file_exist.sh

---
+++ check_file_exist.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Ce script vérifie si un fichier donné existe ou non
+
+# Demande du nom du fichier
+read -p "Entrez le nom du fichier : " filename
+
+# Vérification de l'existence du fichier
+if [ -f "$filename" ]; then
+    echo "Le fichier '$filename' existe."
+else
+    echo "Le fichier '$filename' n'existe pas."
+
