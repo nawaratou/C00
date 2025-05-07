c71c3b44d77dcb5897d0a95823112e282fa88fe3
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-07 15:19:19 +0100

    voir le ficher

--- create_directory.sh
+++ create_directory.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
+
 # Ce script crée un répertoire nommé test_directory
-
-# Utilisation de la commande `mkdir` pour créer le répertoire
 mkdir -p test_directory
-
-# Affichage d'un message pour confirmer la création du répertoire
-echo "Le répertoire 'test_directory' a été créé (ou existe déjà)."
