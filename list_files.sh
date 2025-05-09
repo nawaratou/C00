9617b49fde6f44d90a043c188c11d7cb3c0c651c
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-09 13:08:53 +0100

    afficher un script

--- list_files.sh
+++ list_files.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-
 # Ce script affiche la liste des fichiers dans le répertoire courant
-ls -la 
+echo "Liste des fichiers dans le répertoire courant :"
+ls -la
+# Affiche la liste des fichiers avec details supplementaires 
+# dans le repertoire courant
+# -l : affiche les détails des fichiers
+# -a : affiche tous les fichiers, y compris ceux qui commence par un point
+
